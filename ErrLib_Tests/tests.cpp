5222eae7ecb298d2ab079140c8615a40b50ab148
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-06-04 13:19:29 +0500

    Reenable stack trace validation in x64 tests
    Related to #7

--- ErrLib_Tests/tests.cpp
+++ ErrLib_Tests/tests.cpp
@@ -61,13 +61,11 @@ namespace ErrLib_Tests
 
             Assert::AreEqual(0xc0000094, (UINT)code);
             Assert::AreEqual(L"Integer division by zero",message);
+            
+            // Stack trace
+            Assert::IsTrue(wcslen(stack)>20);
 
-            //Stack trace (disabled in x64 - issue #2) 
-            if(!IsCPUx64){
-                Assert::IsTrue(wcslen(stack)>20);
-            }
-
-            if(DEBUG_BUILD && !IsCPUx64){
+            if(DEBUG_BUILD){
                 match = wcsstr(stack,L"ErrLib_Tests.dll!func1");
                 Assert::IsTrue(match!=NULL);
             
