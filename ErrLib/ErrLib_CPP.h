99936e9888519e73145f27e5c70191fe18c2330f
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-06-12 23:26:34 +0500

    Add more APIs
    Add PrintStackTrace & Log methods

--- ErrLib/ErrLib_CPP.h
+++ ErrLib/ErrLib_CPP.h
@@ -5,44 +5,63 @@
 #include "ErrLib.h"
 #include <string>
 
-namespace ErrLib{
+const DWORD ERRLIB_CPP_EXCEPTION = 0xC0400002;
 
-const int CppExceptionCode = 1;
+namespace ErrLib{
 
 class Exception : public std::exception{
 private:
     CONTEXT _context;
     std::wstring _msg;
-    int _code;
+    DWORD _code;
     void* _data;
 
 public:
 
-    Exception():_msg(L""),_code(CppExceptionCode),_data(nullptr){
+    Exception():_msg(L""),_code(ERRLIB_CPP_EXCEPTION),_data(nullptr){
         RtlCaptureContext(&_context);
     }
 
-    Exception(const std::wstring& message):_msg(message),_code(CppExceptionCode),_data(nullptr){
+    Exception(const std::wstring& message):_msg(message),_code(ERRLIB_CPP_EXCEPTION),_data(nullptr){
         RtlCaptureContext(&_context);
     }
 
-    Exception(const std::wstring& message, int code, void* data):_msg(message),_code(code),_data(data){
+    Exception(const std::wstring& message, DWORD code, void* data):_msg(message),_code(code),_data(data){
         RtlCaptureContext(&_context);
     }
     
-    std::wstring GetMessage(){return _msg;}
+    std::wstring GetMsg(){return _msg;}
 
     int GetCode(){return _code;}
 
     void* GetData(){return _data;}
 
-    void GetMessageText(WCHAR* pOutput, size_t cch){
+    void GetMessageText(WCHAR* pOutput, int cch){
         const WCHAR* pChars = _msg.c_str();
         wcscpy_s(pOutput, cch, pChars);
     }
 
     void GetContext(CONTEXT* pOutput){
         memcpy(pOutput, &_context, sizeof(CONTEXT));
+    }
+
+    void PrintStackTrace(WCHAR* pOutput, int cch){
+        ErrLib_PrintStack(&_context, pOutput, cch);
+    }
+
+    std::wstring PrintStackTrace(){
+        WCHAR buf[ErrLib_StackLen]=L"";
+        this->PrintStackTrace(buf, ErrLib_StackLen);
+        return std::wstring(buf);
+    }
+
+    void Log(bool visible){
+        BOOL bVisible;
+
+        if (visible) bVisible = TRUE;
+        else bVisible = FALSE;
+
+        ErrLib_LogExceptionInfo(_code, _msg.c_str(), this->PrintStackTrace().c_str(), bVisible);
     }
 };
 
